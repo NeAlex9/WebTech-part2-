0438ad1ced032ef138c4ce5e4947d8a42b315931
Алексей Невструев <leha.nevstruev.01@gmail.com>
2021-09-23 12:24:59 +0300

    Make shell sort

--- src/laba1/arrays/operations/ShellSorter.java
+++ src/laba1/arrays/operations/ShellSorter.java
@@ -2,10 +2,28 @@ package laba1.arrays.operations;
 
 public class ShellSorter {
     public static void main(String[] args) {
-        double[] arr = new double[]{2.3, 3, 4.2, 5, 7, 11};
+        double[] arr = new double[]{3, 9.8, 0.2, 1, 7, 11};
+        ShellSort(arr);
+        for (double element : arr) {
+            System.out.print(element + " ");
+        }
     }
 
-    private static void sortByShell(double[] arr) {
+    public static void ShellSort(double[] array) {
+        int i = 0;
+        while (i < array.length - 1) {
+            if (array[i] <= array[i + 1]) {
+                i++;
+            } else {
+                swap(array, i, i+1);
+                i = i > 0 ? i - 1 : i;
+            }
+        }
+    }
 
+    private static void swap(double[] array, int a, int b) {
+        double buf = array[a];
+        array[a] = array[b];
+        array[b] = buf;
     }
 }
